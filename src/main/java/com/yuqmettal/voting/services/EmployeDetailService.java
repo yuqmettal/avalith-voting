e5c8ca26e4b54d4a01650591119c5ed1c405cce3
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-08 10:24:48 -0500

    Add service annotation

--- src/main/java/com/yuqmettal/voting/services/EmployeDetailService.java
+++ src/main/java/com/yuqmettal/voting/services/EmployeDetailService.java
@@ -1,5 +1,6 @@
 package com.yuqmettal.voting.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +14,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeDetailService implements UserDetailsService {
 
     @Autowired
@@ -22,14 +25,14 @@ public class EmployeDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Employe employe = userRepository.findByUsername(username);
-        List<GrantedAuthority> authorities = employe.getRoles().stream()
-					.map(role -> new SimpleGrantedAuthority(role.getName()))
-                    .collect(Collectors.toList());
+        // List<GrantedAuthority> authorities = employe.getRoles().stream()
+		// 			.map(role -> new SimpleGrantedAuthority(role.getName()))
+        //             .collect(Collectors.toList());
         return new User(
             employe.getUsername(),
             employe.getPassword(),
             employe.getEnabled(),
-            true, true, true, authorities
+            true, true, true, new ArrayList<>()
         );
     }
     
