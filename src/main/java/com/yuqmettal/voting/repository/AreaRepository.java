cbcf0b124eeb75d6a3b35735018dee608b4a0235
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-09 10:56:42 -0500

    Add area repository

---
+++ src/main/java/com/yuqmettal/voting/repository/AreaRepository.java
@@ -0,0 +1,8 @@
+package com.yuqmettal.voting.repository;
+
+import com.yuqmettal.voting.model.Area;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area, Long> {
+}
