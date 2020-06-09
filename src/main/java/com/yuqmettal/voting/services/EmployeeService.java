4680f1c00f81a568df6f4c8982d2094433bf707f
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-09 09:33:46 -0500

    Add validation create Employee

--- src/main/java/com/yuqmettal/voting/services/EmployeeService.java
+++ src/main/java/com/yuqmettal/voting/services/EmployeeService.java
@@ -7,14 +7,22 @@ import java.util.stream.Collectors;
 import com.yuqmettal.voting.entity.EmployeeEntity;
 import com.yuqmettal.voting.model.Employee;
 import com.yuqmettal.voting.repository.EmployeeRepository;
+import com.yuqmettal.voting.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public List<EmployeeEntity> getAll() {
         return ((List<Employee>) this.employeeRepository.findAll()).stream()
@@ -23,6 +31,12 @@ public class EmployeeService {
     }
 
     public Employee create(Employee employee) {
+        Employee userEmployee = this.userRepository.findByUsername(employee.getUsername());
+        if (userEmployee != null) {
+            throw new RuntimeException("Employee already exist.");
+        }
+        employee.setEnabled(true);
+        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
         return this.employeeRepository.save(employee);
     }
 
