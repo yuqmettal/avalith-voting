0b481d28133595a68a20fe888bb79213b7ab0454
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-08 19:42:32 -0500

    Remove department references

--- src/main/java/com/yuqmettal/voting/model/Employee.java
+++ src/main/java/com/yuqmettal/voting/model/Employee.java
@@ -3,17 +3,15 @@ package com.yuqmettal.voting.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -48,19 +46,8 @@ public class Employee implements Serializable {
 
     private Integer intents;
 
-    @ManyToOne(optional = false, cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-    private Department department;
-
     public Integer getIntents() {
         return intents;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
     }
 
     public void setIntents(Integer intents) {
