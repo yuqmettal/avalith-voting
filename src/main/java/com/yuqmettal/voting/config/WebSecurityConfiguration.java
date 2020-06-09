0be616ac08d5b9d356165ba9c21bea50b9040bf5
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-09 09:32:39 -0500

    Add security roles to access to emplyee controller

--- src/main/java/com/yuqmettal/voting/config/WebSecurityConfiguration.java
+++ src/main/java/com/yuqmettal/voting/config/WebSecurityConfiguration.java
@@ -3,14 +3,15 @@ package com.yuqmettal.voting.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
@@ -20,18 +21,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests()
-        .antMatchers("/").permitAll()
-        .antMatchers(HttpMethod.GET, "/employee").hasAnyRole("ADMIN", "EMPLOYEE")
-        .antMatchers(HttpMethod.POST, "/employee").hasAnyRole("ADMIN")
-        .anyRequest().authenticated().and().httpBasic();
+        .csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/employee").hasRole("ADMIN")
+		.antMatchers("/employee/*").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and().httpBasic();
     }
 }
