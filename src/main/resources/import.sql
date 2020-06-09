e4c9362351b5714f415080a7529a1b1423466ecc
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-08 19:44:59 -0500

    Update preloaded data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,13 @@
 
-INSERT INTO departments (name) VALUES ('IT');
-INSERT INTO departments (name) VALUES ('SALES');
+INSERT INTO areas (name) VALUES ('Team playe');
+INSERT INTO areas (name) VALUES ('Technical referent');
+INSERT INTO areas (name) VALUES ('Key Player');
+INSERT INTO areas (name) VALUES ('Client satisfaction');
+INSERT INTO areas (name) VALUES ('Motivation');
+INSERT INTO areas (name) VALUES ('Fun');
 
-INSERT INTO employes (username, password, enabled, first_name, last_name, email, department_id) VALUES ('marco','$2a$04$Bn3UVpMaH65PWb5yVqial.xFzFgcucvoR5sg4tpA/wbprGFFi7sxm',true, 'Marco', 'Yuquilima','myuquilima@yuqmettal.com', 1);
-INSERT INTO employes (username, password, enabled, first_name, last_name, email, department_id) VALUES ('admin','$2a$04$t78hYPjiY2enNx0HwpSK3.aZmDUXArthOcD6a7CbogeKEFY22H7DO',true, 'John', 'Doe','jhon.doe@bolsadeideas.com', 2);
+INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('marco','$2a$04$Bn3UVpMaH65PWb5yVqial.xFzFgcucvoR5sg4tpA/wbprGFFi7sxm',true, 'Marco', 'Yuquilima','myuquilima@yuqmettal.com');
+INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('admin','$2a$04$t78hYPjiY2enNx0HwpSK3.aZmDUXArthOcD6a7CbogeKEFY22H7DO',true, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO roles (name) VALUES ('ROLE_EMPLOYEE');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
