53fb6a587d1574b740486661e9426dd826352065
Marco Yuquilima <marcoyuqui@hotmail.com>
2020-06-09 16:02:05 -0500

    Update user passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,9 +8,9 @@ INSERT INTO areas (name) VALUES ('Fun');
 
 INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('marco','$2a$10$O.rvunSHecxX7irLv2vlz.RpMBSKEJtJeHnmy/uURk8nfFmeYGq7K',true, 'Marco', 'Yuquilima','myuquilima@yuqmettal.com');
 INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('admin','$2a$10$cwPHZMy1YpIZ2hKqpdgRSev2qONVi8ro.98ZAFjvyPUbeKM2ugerK',true, 'John', 'Doe','admin@yuqmettal.com');
-INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('hugo','$2a$10$cwPHZMy1YpIZ2hKqpdgRSev2qONVi8ro.98ZAFjvyPUbeKM2ugerK',true, 'Hugo', 'Doe','hugo@yuqmettal.com');
-INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('paco','$2a$10$cwPHZMy1YpIZ2hKqpdgRSev2qONVi8ro.98ZAFjvyPUbeKM2ugerK',true, 'Paco', 'Doe','paco@yuqmettal.com');
-INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('luis','$2a$10$cwPHZMy1YpIZ2hKqpdgRSev2qONVi8ro.98ZAFjvyPUbeKM2ugerK',true, 'Luis', 'Doe','luis@yuqmettal.com');
+INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('hugo','$2a$10$qjW0IC0NBUMd7nnG4pazP./X9IxnkVcB9VoipjgFaAZseNlAxycwC',true, 'Hugo', 'Doe','hugo@yuqmettal.com');
+INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('paco','$2a$10$rB1Y9.1ueRfjjw.l/bSkve/bhYsssyD4CL94IpfUVgS/J/FfQEGG2',true, 'Paco', 'Doe','paco@yuqmettal.com');
+INSERT INTO employes (username, password, enabled, first_name, last_name, email) VALUES ('luis','$2a$10$URLFd2ONSmo9P1W7D1rO2usBsQIbZmCiVel1NTcAMMcVTCTIfRR5e',true, 'Luis', 'Doe','luis@yuqmettal.com');
 
 INSERT INTO roles (name) VALUES ('ROLE_EMPLOYEE');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
